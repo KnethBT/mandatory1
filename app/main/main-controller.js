34e2b0eac9d3c311bcdaa5de297d133a436a9e11
KnethBT <Kenneth2503@hotmail.com>
2015-11-06 09:12:15 +0100

    Søg i table virker

--- app/main/main-controller.js
+++ app/main/main-controller.js
@@ -134,5 +134,22 @@
                 //Init
                 getContents(true);
 
+
+                (function ($)
+                {
+
+                    $('#filter').keyup(function ()
+                    {
+                        var rex = new RegExp($(this).val(), 'i');
+                        $('.searchable tr').hide();
+                        $('.searchable tr').filter(function ()
+                        {
+                            return rex.test($(this).text());
+                        }).show();
+
+                    })
+
+                }(jQuery));
+
             }]);
 }());
